7aac8e53a103b517f56ddb36d546d41288ad4ca4
Marcos Cassinerio <marcos.cassinerio@mangoodev.com>
2020-04-23 20:52:13 -0300

    changed language glist

--- GList/glist.c
+++ GList/glist.c
@@ -2,200 +2,200 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-GList glist_crear() {
+GList glist_create() {
     return NULL;
 }
 
-void glist_destruir(GList lista, Destruir function) {
-  GNodo* nodoAux;
-  GNodo* anterior = lista->ant;
-  for (; lista != anterior; lista = nodoAux) {
-    nodoAux = lista->sig;
-    function(lista->dato);
-    free(lista);
+void glist_destroy(GList list, Destroy function) {
+  GNode* auxNode;
+  GNode* anterior = list->ant;
+  for (; list != anterior; list = auxNode) {
+    auxNode = list->sig;
+    function(list->data);
+    free(list);
   }
-  function(lista->dato);
-  free(lista);
+  function(list->data);
+  free(list);
 }
 
-void glist_destroy_copy(GList lista) {
-  GNodo* nodoAux;
-  GNodo* anterior = lista->ant;
-  for (; lista != anterior; lista = nodoAux) {
-    nodoAux = lista->sig;
-    free(lista);
+void glist_destroy_copy(GList list) {
+  GNode* auxNode;
+  GNode* anterior = list->ant;
+  for (; list != anterior; list = auxNode) {
+    auxNode = list->sig;
+    free(list);
   }
-  free(lista);
+  free(list);
 }
 
-GNodo* glist_pop(GList* lista, int pos) {
-  GNodo* nodo = glist_crear();
-  if (lista) {
-    if ((*lista)->sig == *lista) {
-      nodo = *lista;
-      lista = NULL;
+GNode* glist_pop(GList* list, int pos) {
+  GNode* node = glist_crear();
+  if (list) {
+    if ((*list)->sig == *list) {
+      node = *list;
+      list = NULL;
     } else {
-      nodo = *lista;
-      for (; nodo->sig != (*lista) && pos != 0; nodo = nodo->sig, --pos);
+      node = *list;
+      for (; node->sig != (*list) && pos != 0; node = node->sig, --pos);
       if (pos == 0) {
-        *lista = nodo->sig;
-        nodo->ant->sig = nodo->sig;
-        nodo->sig->ant = nodo->ant;
-        nodo->sig = nodo;
-        nodo->ant = nodo;
+        *list = node->sig;
+        node->ant->sig = node->sig;
+        node->sig->ant = node->ant;
+        node->sig = node;
+        node->ant = node;
       }
     }
   }
-  return nodo;
+  return node;
 }
 
-GList glist_concat(GList lista1, GList lista2) {
-  if (!lista1)
-    return lista2;
-  if (!lista2)
-    return lista1;
-  lista1->ant->sig = lista2;
-  lista2->ant->sig = lista1;
-  GList temp = lista1->ant;
-  lista1->ant = lista2->ant;
-  lista2->ant = temp;
-  return lista1;
+GList glist_concat(GList list1, GList list2) {
+  if (!list1)
+    return list2;
+  if (!list2)
+    return list1;
+  list1->ant->sig = list2;
+  list2->ant->sig = list1;
+  GList temp = list1->ant;
+  list1->ant = list2->ant;
+  list2->ant = temp;
+  return list1;
 }
 
-GList glist_insertar_final(GList lista, void* dato) {
-  GNodo* nodo = malloc(sizeof(GNodo));
-  nodo->dato = dato;
-  nodo->ant = nodo;
-  nodo->sig = nodo;
+GList glist_insert_last_position(GList list, void* data) {
+  GNode* node = malloc(sizeof(GNode));
+  node->data = data;
+  node->ant = node;
+  node->sig = node;
 
-  return glist_concat(lista, nodo);
+  return glist_concat(list, node);
 }
 
-GList glist_copy(GList lista) {
+GList glist_copy(GList list) {
   GList newList = glist_crear();
-  GNodo* node = lista;
-  if (lista) {
-    for (;node->sig != lista; node = node->sig)
-      newList = glist_insertar_final(newList, node->dato);
-    newList = glist_insertar_final(newList, node->dato);
+  GNode* node = list;
+  if (list) {
+    for (;node->sig != list; node = node->sig)
+      newList = glist_insertar_final(newList, node->data);
+    newList = glist_insertar_final(newList, node->data);
   }
   return newList;
 }
 
-void glist_imprimir_archivo(FILE* salida, GList lista, FuncionVisitante function) {
-  if (lista) {
-    function(salida, lista->dato);
-    GNodo* nodoAux = lista->sig;
-    for (; nodoAux != lista; nodoAux = nodoAux->sig)
-      function(salida, nodoAux->dato);
+void glist_imprimir_archivo(FILE* output, GList list, VisitorFunction function) {
+  if (list) {
+    function(output, list->data);
+    GNode* auxNode = list->sig;
+    for (; auxNode != list; auxNode = auxNode->sig)
+      function(output, auxNode->data);
   }
 }
 
-void glist_swap(GNodo* nodo1, GNodo* nodo2) {
-  void* dato = nodo2->dato;
-  nodo2->dato = nodo1->dato;
-  nodo1->dato = dato;
+void glist_swap(GNode* node1, GNode* node2) {
+  void* data = node2->data;
+  node2->data = node1->data;
+  node1->data = data;
 }
 
-GList glist_selection_sort(GList lista, Compara function) {
-  if (lista) {
-    GNodo* nodoAux = lista;
-    for (; nodoAux != lista->ant; nodoAux = nodoAux->sig) {
-      GNodo* nodoASwapear = nodoAux;
-      GNodo* nodo = nodoAux->sig;
-      for (; nodo != lista; nodo = nodo->sig) 
-        if (function(nodo->dato, nodoASwapear->dato) < 0)
-          nodoASwapear = nodo;
-      if (nodoAux != nodoASwapear) {
-        glist_swap(nodoAux, nodoASwapear);
+GList glist_selection_sort(GList list, Compare function) {
+  if (list) {
+    GNode* auxNode = list;
+    for (; auxNode != list->ant; auxNode = auxNode->sig) {
+      GNode* nodeASwapear = auxNode;
+      GNode* node = auxNode->sig;
+      for (; node != list; node = node->sig) 
+        if (function(node->data, nodeASwapear->data) < 0)
+          nodeASwapear = node;
+      if (auxNode != nodeASwapear) {
+        glist_swap(auxNode, nodeASwapear);
       }
     }
   }
-  return lista;
+  return list;
 }
 
-GList glist_insertion_sort(GList lista, Compara function) {
-  if (lista) {
-    GNodo* nodoAux = lista->sig;
+GList glist_insertion_sort(GList list, Compare function) {
+  if (list) {
+    GNode* auxNode = list->sig;
     do {
-      GNodo* nodo = nodoAux->ant;
-      GNodo* nodoHastaSwapear = glist_crear();
-      for (; function(nodo->dato, nodoAux->dato) > 0 && nodo != lista->ant;
-          nodo = nodo->ant);
-      if (function(nodo->dato, nodoAux->dato) != 1) {
-        nodoHastaSwapear = nodo->sig;
+      GNode* node = auxNode->ant;
+      GNode* nodeHastaSwapear = glist_crear();
+      for (; function(node->data, auxNode->data) > 0 && node != list->ant;
+          node = node->ant);
+      if (function(node->data, auxNode->data) != 1) {
+        nodeHastaSwapear = node->sig;
       } else {
-        nodoHastaSwapear = nodo;
+        nodeHastaSwapear = node;
       }
-      GNodo* nuevaUltimaPos = nodoAux->sig;
-      for (; nodoAux != nodoHastaSwapear; nodoAux = nodoAux->ant)
-        glist_swap(nodoAux, nodoAux->ant);
-      nodoAux = nuevaUltimaPos;
-    } while (nodoAux != lista);
+      GNode* nuevaUltimaPos = auxNode->sig;
+      for (; auxNode != nodeHastaSwapear; auxNode = auxNode->ant)
+        glist_swap(auxNode, auxNode->ant);
+      auxNode = nuevaUltimaPos;
+    } while (auxNode != list);
   }
-  return lista;
+  return list;
 }
 
-GList glist_merge(GList lista1, GList lista2, Compara function) {
-  if (!lista1)
-    return lista2;
-  if (!lista2)
-    return lista1;
-  if (function(lista1->dato, lista2->dato) < 0) {
-    if (lista1 == lista1->sig) {
-      lista1 = glist_concat(lista1, lista2);
+GList glist_merge(GList list1, GList list2, Compare function) {
+  if (!list1)
+    return list2;
+  if (!list2)
+    return list1;
+  if (function(list1->data, list2->data) < 0) {
+    if (list1 == list1->sig) {
+      list1 = glist_concat(list1, list2);
     } else {
-      GNodo* nodoAux = glist_pop(&lista1, 0);
-      lista1 = glist_merge(lista1, lista2, function);
-      lista1 = glist_concat(nodoAux, lista1);
+      GNode* auxNode = glist_pop(&list1, 0);
+      list1 = glist_merge(list1, list2, function);
+      list1 = glist_concat(auxNode, list1);
     }
-    return lista1;
+    return list1;
   } else {
-    if (lista2 == lista2->sig) {
-      lista2 = glist_concat(lista2, lista1);
+    if (list2 == list2->sig) {
+      list2 = glist_concat(list2, list1);
     } else {
-      GNodo* nodoAux = glist_pop(&lista2, 0);
-      lista2 = glist_merge(lista1, lista2, function);
-      lista2 = glist_concat(nodoAux, lista2);
+      GNode* auxNode = glist_pop(&list2, 0);
+      list2 = glist_merge(list1, list2, function);
+      list2 = glist_concat(auxNode, list2);
     }
-    return lista2;
+    return list2;
   }
 }
 
-GList glist_split(GList lista) {
-  GNodo* nodo1 = lista;
-  GNodo* nodo2 = lista;
-  for (; nodo1->sig != lista && nodo1->sig->sig != lista;
-      nodo1 = nodo1->sig->sig, nodo2 = nodo2->sig);
-  GList listaNueva = nodo2->sig;
-  lista->ant->sig = listaNueva;
-  listaNueva->ant = lista->ant;
-  lista->ant = nodo2;
-  nodo2->sig = lista;
-  return listaNueva;
+GList glist_split(GList list) {
+  GNode* node1 = list;
+  GNode* node2 = list;
+  for (; node1->sig != list && node1->sig->sig != list;
+      node1 = node1->sig->sig, node2 = node2->sig);
+  GList listNueva = node2->sig;
+  list->ant->sig = listNueva;
+  listNueva->ant = list->ant;
+  list->ant = node2;
+  node2->sig = list;
+  return listNueva;
 }
 
-GList glist_merge_sort(GList lista, Compara function) {
-  if (!lista || lista->sig == lista)
-    return lista;
-  GList lista2 = glist_split(lista);
+GList glist_merge_sort(GList list, Compare function) {
+  if (!list || list->sig == list)
+    return list;
+  GList list2 = glist_split(list);
 
-  lista = glist_merge_sort(lista, function);
-  lista2 = glist_merge_sort(lista2, function);
+  list = glist_merge_sort(list, function);
+  list2 = glist_merge_sort(list2, function);
 
-  return glist_merge(lista, lista2, function);
+  return glist_merge(list, list2, function);
 }
 
-void glist_test_sort_algorithm(char* nombreArchivo, GList lista, SortAlgorithm sort,
-  Compara functionCompare, FuncionVisitante functionVisit) {
-  GList copiaLista = glist_copy(lista);
+void glist_test_sort_algorithm(char* nombreArchivo, GList list, SortAlgorithm sort,
+  Compare functionCompare, VisitorFunction functionVisit) {
+  GList copiaLista = glist_copy(list);
   clock_t t = clock();
   copiaLista = sort(copiaLista, functionCompare);
   t = clock() - t;
   double tiempoSegundos = ((double)t)/CLOCKS_PER_SEC;
-  FILE* salida = fopen(nombreArchivo, "w");
-  fprintf(salida, "Tiempo: %lf\n\n", tiempoSegundos);
-  glist_imprimir_archivo(salida, copiaLista, functionVisit);
-  fclose(salida);
+  FILE* output = fopen(nombreArchivo, "w");
+  fprintf(output, "Tiempo: %lf\n\n", tiempoSegundos);
+  glist_imprimir_archivo(output, copiaLista, functionVisit);
+  fclose(output);
   glist_destroy_copy(copiaLista);
 }
